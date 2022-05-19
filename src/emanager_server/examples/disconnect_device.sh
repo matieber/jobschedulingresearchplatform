b02796319c563a3c5c1bb9ff496cf37c142f1d67
mhirsch <matias.hirsch@gmail.com>
2022-05-18 21:19:50 -0300

    removing examples folder

--- src/emanager_server/examples/disconnect_device.sh
+++
@@ -1,1 +0,0 @@
-curl -i -X PUT -H 'Content-Type: application/json' -d '{"requiredVirtualConnectionState": "disconnected"}' http://localhost:1080/connection/motorola_moto_g6
--- src/emanager_server/examples/put_energy_device.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-params = "?requiredEnergyState=discharging&slotId=1"
-device = "motorola_moto_g9_play"
-r = requests.put("http://localhost:1080/energy/" + device + params)
-print(r.text)
